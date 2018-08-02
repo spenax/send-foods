c1ecb7a316401d4eb287f7febe676db7ff768e0d
spenax <spenax@aol.com>
2018-08-02 15:30:22 -0400

    add trend charts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,12 @@
 class MessagesController < ApplicationController
   helper_method :messages_helper
+
+
   def index
-    @emoji_list = Message.ordered_tally[0..9]
+    @hourly_list = Message.hourly_tally[0..9]
+    @trends = Message.last_three(@hourly_list)
+
+
 
   end
 
@@ -9,16 +14,16 @@ class MessagesController < ApplicationController
     @emoji = params[:emoji]
     @message = Message.find_by(emoji: @emoji)
     totals = Message.daily_totals(@emoji)
-
-    @sun = totals[0].size
-    @mon = totals[1].size
-    @tues = totals[2].size
-    @wed = totals[3].size
-    @thur = totals[4].size
-    @fri = totals[5].size
-    @sat = totals[6].size
+    egg_totals = Message.daily_totals("🍆")
 
 
+    @day_zero = totals[0]
+    @day_one = totals[1]
+    @day_two = totals[2]
+
+    @egg_day_zero = egg_totals[0]
+    @egg_day_one = egg_totals[1]
+    @egg_day_two = egg_totals[2]
 
 
   end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,29 @@
 class Message < ApplicationRecord
   validates_presence_of :emoji
+  scope :last_hour, -> { where('created_at > ?', 1.hour.ago)}
+  scope :last_hour_plus_one, -> { where('created_at < ? AND created_at > ?', 1.hour.ago, 2.hours.ago)}
+  scope :last_hour_plus_two, -> { where('created_at < ? AND created_at > ?', 2.hours.ago, 3.hours.ago)}
+
+  scope :last_three_hours_combined, -> { where('created_at > ?', 3.hours.ago)}
+  # Probably bad becauce they will return all if nil?
+  # This might not be great because these are periods of 24 hours instead of calendar days...Might be cool as a line graph
+  # At that point you could create a comparison action
+
+  # 1 day ago
+  scope :last_day, -> { where('created_at < ? AND created_at > ?', Time.now, 1.days.ago)}
+  # 2 days ago
+  scope :last_day_plus_one, -> { where('created_at < ? AND created_at > ?', 1.day.ago, 2.days.ago)}
+  # 3 days ago
+  scope :last_day_plus_two, -> { where('created_at < ? AND created_at > ?', 2.day.ago, 3.days.ago)}
+  # 4 days ago
+  scope :last_day_plus_three, -> { where('created_at < ? AND created_at > ?', 3.day.ago, 4.days.ago)}
+  # 5 days ago
+  scope :last_day_plus_four, -> { where('created_at < ? AND created_at > ?', 4.day.ago, 5.days.ago)}
+  # 6 days ago
+  scope :last_day_plus_five, -> { where('created_at < ? AND created_at > ?', 5.day.ago, 6.days.ago)}
+  # 7 days ago
+  scope :last_day_plus_six, -> { where('created_at < ? AND created_at > ?', 6.day.ago, 7.days.ago)}
+
 
 
   # Make request to API
@@ -9,10 +33,22 @@ class Message < ApplicationRecord
 
   class << self
     BASE_URL = "https://venmo.com/api/v5/public"
-    def ordered_tally
-      unordered_emoji_counts = Message.group(:emoji).count
-      ordered_emoji_counts = unordered_emoji_counts.sort_by {|k, v| v}
-      ordered_emoji_counts.reverse
+    def alltime_tally
+      @all_time_counts = self.group(:emoji).count
+      self.reverse_sort(@all_time_counts)
+
+    end
+
+    def hourly_tally
+      @hourly_counts = self.last_hour.group(:emoji).count
+      self.reverse_sort(@hourly_counts)
+
+    end
+
+    def reverse_sort (group)
+      @ordered_emoji_counts = group.sort_by {|k, v| v}
+      @ordered_emoji_counts.reverse
+
     end
 
     def req(url)
@@ -32,13 +68,13 @@ class Message < ApplicationRecord
 
 
     def import_latest
-      recent_trans = Message.req(BASE_URL) #array of hashes
+      recent_trans = self.req(BASE_URL) #array of hashes
       recent_trans.each do |tran|
-        @unique_emoji = Message.parse_emoji(tran["message"])
+        @unique_emoji = self.parse_emoji(tran["message"])
         if @unique_emoji
           @unique_emoji.each do |emoji|
             @name = emoji.name
-            Message.create(emoji: emoji)
+            self.create(emoji: emoji)
           end
         end
       end
@@ -46,18 +82,56 @@ class Message < ApplicationRecord
 
     # Return array of daily totals for Sunday through Saturday of a type of emoji...ex: poop
     def daily_totals(emoji)
-      #test....Leave for now and then at the last week.
-      empty_week = { 0 => [], 1 => [], 2 => [], 3 => [], 4 => [], 5 => [], 6 => [] }
 
-      @emoji_group = Message.where(emoji: emoji).pluck(:created_at)
-      week_occurances = @emoji_group.group_by(&:wday)
-
-      total_week_occurances = empty_week.merge(week_occurances)
-
-      total_week_occurances
-
+      @day_zero = self.last_day.where(emoji: emoji).pluck(:created_at).size
+      @day_one = self.last_day_plus_one.where(emoji: emoji).pluck(:created_at).size
+      @day_two = self.last_day_plus_two.where(emoji: emoji).pluck(:created_at).size
+      some_days = [@day_zero, @day_one, @day_two]
 
     end
+
+
+    #return 1 if the average rate of change of set of points is increasing at an increasing rate
+
+    def rate_of_change(p_two, p_one, p_zero)
+
+      #this is the most recent slope
+      @l_zero = p_zero - p_one
+
+      #this is the older slope
+      @l_one = p_one - p_two
+
+      if (@l_zero.abs > @l_one.abs) && (@l_zero > 0)
+        1
+      #elsif @l_zero.abs < @l_one.abs
+        #-1
+      else
+        0
+      end
+    end
+
+    def last_three (emoji_list)
+      past_three = self.last_three_hours_combined
+      trends = []
+      emoji_list.each do |emoji_pair|
+        emoji = emoji_pair[0]
+        @first_hour_ago = past_three.last_hour.where(emoji: emoji).pluck(:created_at).length
+        @second_hour_ago = past_three.last_hour_plus_one.where(emoji: emoji).pluck(:created_at).length
+        @third_hour_ago = past_three.last_hour_plus_two.where(emoji: emoji).pluck(:created_at).length
+        rate = rate_of_change(@third_hour_ago, @second_hour_ago, @first_hour_ago)
+        trends.push(rate)
+      end
+      trends
+
+    end
+
+
+
+
+
+
+
+
 
   end
 
